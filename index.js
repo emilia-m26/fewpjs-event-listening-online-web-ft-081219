26b7c78d805564871425daf15a0684c0b0b50090
Emilia <emilia.mena26@gmail.com>
2019-11-30 17:41:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    //node that will be doing the listening
+    const input = document.getElementById('input');
+    //will be doing the listening
+    //first arg - event name, second arg -callback function
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+    });
 }
